8dca951ce8a34e8b0c034f2527ff112ccbf88e1d
Nullifier225 <55820583+Nullifier225@users.noreply.github.com>
2021-07-24 13:45:57 +0530

    added all the features

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ def landing():
 @app.route("/" , methods = ['GET', 'POST']) 
 def getlink():
     youtube_link = request.form['link']
-    vid = pd.video_download(youtube_link)
-    return render_template('landing.html',vid = vid)
+    choice = request.form['choice']
+    my_video =  youtube_link.streams.filter(only_audio = choice)
+    my_video.download('~/Downloads') 
+    return render_template('landing.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
